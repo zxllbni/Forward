f931a967f66e6f5f5b73936cc30b11c27e3e077c
zxllbni <webhostooo70@gmail.com>
2024-07-02 10:10:37 +0530

    Update BotConfig.py

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -4,7 +4,7 @@ class Config(object):
     API_HASH = environ.get("API_HASH", "93bf5cde23435bb236066dcd7358ae6a")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7250959737:AAEoq5PaAlZU5e83utVh6QUEX75NTgiYpIQ")
     STRING_SESSION = environ.get("STRING_SESSION", "1BVtsOKEBu8YRuj9QiEDi_2yvFCi-c7raOlzaiMx58dsL6sbCmbuXuV8YZyjhAIMff4sNgtFulzeb_h_uaz79mJFNmzaAwABWwiY9de0xf4rZdSFdGmcpCFh8qcQRnJM6sA3WS1JPth2L88WnYI6oSrEOkMBAy6jmCzaoo4C4P7k7ekxo4jkDA8XuDS-uN4UB-xVn0y9m2R_udqgjNygkI8w9k3yKrATAPXrQ0ETrr-6g3FGXb0ynpxGxqoRQrjG-9tYPwwBMVm57httR32-kStnaxM1T3X_SkI0NPH_3ff69U0HMyiV9PmmFN9_t3FPFye7hDRhJPDy4Pk5_CyyoDkf_CS4XA8o=")
-    SUDO_USERS = environ.get("SUDO_USERS", "6467503244")
+    SUDO_USERS = environ.get("SUDO_USERS", "6467503244,6473618718,7055312436")
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
     💢 **ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ɪɴ ᴛʜᴇ ʙᴏᴛ ᴀʀᴇ:**
